fad705ebfd7d0ffcf0507b7fa85b63efb512f3d6
Anderson Andrade <anderson_andrade_@outlook.com>
2020-05-15 17:19:41 -0300

    {UPDATE: creating the login page}

---
+++ src/pages/Logon/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './styles.css';
+
+// images
+import herosImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
+import { FiLogIn } from 'react-icons/fi';
+
+export default function Logon() {
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="logo" />
+
+        <form>
+          <h1>faca o seu logon</h1>
+
+          <input type="text" placeholder="your id" />
+          <button type="submit" className="button">
+            {' '}
+            entrar
+          </button>
+
+          <a href="/register">
+            <FiLogIn size={16} color={'#e02041'} />
+            nao tenho cadastro
+          </a>
+        </form>
+      </section>
+      <img src={herosImg} alt="heros" />
+    </div>
+  );
+}
